5665d2996a0d55495f33cdb21fa256a512c3114e
Siddharth <siddharthgopi@outlook.com>
2023-01-29 23:05:19 +0530

    webpack set up for a multi page app

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,129 @@
+const webpack = require("webpack");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const loader = require("sass-loader");
+var HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
+const LodashModuleReplacementPlugin = require("lodash-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 console.log(isProduction);
 
-const config = {
-  name: "home",
-  entry: "./client/home/home.ts",
-  output: {
-    filename: "home.js",
-    path: path.resolve(__dirname, "public/assets/js"),
-    publicPath: "/public/assets/css/",
-  },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/", "/src/", "/views/"],
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ]
-      },
+const configs = [
+  {
+    entry: { home: "./client/home/home.ts" },
+    output: {
+      filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+      path: path.resolve(__dirname, "public/assets/js"),
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./templates/home/index.pug",
+        filename: "../../../views/home/index.pug",
+        minify: false,
+      }),
+      new HtmlWebpackPugPlugin(),
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
+  {
+    entry: { feed: "./client/feed/index.tsx" },
+    output: {
+      filename: isProduction ? "[name].[contenthash].js" : "[name].js",
+      path: path.resolve(__dirname, "public/assets/js"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./templates/index.pug",
+        filename: "../../../views/feed/index.pug",
+        minify: false,
+      }),
+      new HtmlWebpackPugPlugin(),
+    ],
   },
-  plugins: [
-    ...(isProduction
-      ? [
-          new MiniCssExtractPlugin({
-            filename: "../css/home.css",
-          }),
-        ]
-      : []),
-  ],
-};
+];
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
-  return config;
+  return configs.map((config) => {
+    if (isProduction) {
+      config.mode = "production";
+    } else {
+      config.mode = "development";
+    }
+    const plugins = [
+      ...config.plugins,
+      ...[
+        ...config.plugins,
+        ...(isProduction
+          ? [
+              new MiniCssExtractPlugin({
+                filename: "../css/home.css",
+              }),
+            ]
+          : []),
+        new LodashModuleReplacementPlugin(),
+        new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+      ],
+    ];
+    const entry = config.entry;
+    const output = config.output;
+
+    return {
+      entry,
+      output,
+      ...{
+        devServer: {
+          open: true,
+          host: "localhost",
+        },
+        module: {
+          rules: [
+            {
+              test: /\.(js|jsx)$/,
+              use: "babel-loader",
+              exclude: /node_modules/,
+            },
+            {
+              test: /\.css$/,
+              use: [
+                isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                "css-loader",
+              ],
+            },
+            {
+              test: /\.ts(x)?$/,
+              loader: "ts-loader",
+              exclude: /node_modules/,
+            },
+            {
+              test: /\.scss$/,
+              use: [
+                isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                "css-loader",
+                "sass-loader",
+              ],
+            },
+            {
+              test: /\.svg$/,
+              use: "file-loader",
+            },
+            {
+              test: /\.png$/,
+              use: [
+                {
+                  loader: "url-loader",
+                  options: {
+                    mimetype: "image/png",
+                  },
+                },
+              ],
+            },
+          ],
+        },
+        resolve: {
+          extensions: [".tsx", ".ts", ".js"],
+        },
+      },
+      plugins: plugins,
+    };
+  });
 };
